aab627772e59be26f3434be29196f237c86c5b61
Bhanu Handa <bhanuhanda98@gmail.com>
2025-03-28 19:57:24 +0530

    direct routing fix

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
+import { fileURLToPath } from "url";
+import { resolve, dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, "index.html"),
+                notFound: resolve(__dirname, "404.html"),
+            },
+        },
+    },
 });
